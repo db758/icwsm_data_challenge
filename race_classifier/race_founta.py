b0d1ef694154ec164b061f0a5b8309790f5af1b6
t-davidson <trd54@cornell.edu>
2020-04-02 17:35:37 -0400

    Added final version of race classifier and data

--- race_classifier/race_founta.py
+++ race_classifier/race_founta.py
@@ -1,5 +1,7 @@
-# Python 2.7
+## Python 2.7
 # Run Blodgett model on Founta et al.
+# For necessary files see https://github.com/slanglab/twitteraae
+
 import twokenize, predict
 import csv
 from numpy import array
@@ -8,21 +10,22 @@ predict.load_model()
 
 if __name__ == '__main__':
     data = []
-    with open("data/founta_et_al.csv") as file:
-        rdr = csv.reader(file, delimiter=',')
+    with open("../raw_data/hatespeech_text_label_vote_RESTRICTED_100K.csv") as file:
+        rdr = csv.reader(file, delimiter='\t')
         for row in rdr:
-            id = row[0]
-            text = row[1]
-            label = row[2]
+            #id = row[0]
+            text = row[0]
+            label = row[1]
             annotations = predict.predict(twokenize.tokenize(text.decode("utf-8")))
             try:
-                data.append([id, text, label]+list(annotations))
+                data.append([text, label]+list(annotations))
             except:
-                data.append([id, text, label, -9, -9, -9, -9])
+                data.append([text, label, -9, -9, -9, -9])
 
     with open('data/founta_race_annotated.csv', 'a') as resultsfile:
-        fieldnames = ['id', 'text', 'label', 'afam', 'hisp', 'asian', 'white']
+        fieldnames = ['text', 'label', 'afam', 'hisp', 'asian', 'white']
         writer = csv.writer(resultsfile, lineterminator='\n')
         writer.writerows([fieldnames])
-        for row in data[1:]:
+        for row in data:
             writer.writerows([row])
+
