5dd194181f6c049da8e6f9cf7766aa5e32946dad
Debasmita Bhattacharya <db758@cornell.edu>
2020-04-04 12:42:47 +0800

    re-running STM with additional covariates

---
+++ STM/code/trainingSTM_2.R
@@ -0,0 +1,17 @@
+library(stm)
+setwd("Desktop/icwsm_data_challenge/race_classifier/data/")
+data <- read.csv('founta_race_annotated_2.csv') 
+data <- data[!(data$label == "spam"),]
+data <- data[!(data$afam == -9),]
+processed <- textProcessor(data$cleaned_text, metadata = data.frame(data$label,data$afam, data$white))
+out <- prepDocuments(processed$documents, processed$vocab, processed$meta)
+docs <- out$documents
+vocab <- out$vocab
+meta <-out$meta
+hatespeechFit <- stm(documents = out$documents, vocab = out$vocab,K=25, max.em.its = 200, data = out$meta, init.type = "Spectral") 
+labelTopics(hatespeechFit)
+plot.STM(hatespeechFit,type="summary")
+plot.STM(hatespeechFit, type = "labels")
+
+#might be worth using searchK for K=10, 15, 20, 25, 30, 35 instead of current range 5-25?
+#how to use findThoughts and estimateEffect !! 
